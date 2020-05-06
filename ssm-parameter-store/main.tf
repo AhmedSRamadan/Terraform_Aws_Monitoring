3db0d51fe5824e3acd546110c4bcfe24215a0dea
Ahmed Ramadan <ahmed.sramadan89@gmail.com>
2020-05-06 18:54:00 +0200

    ssm parameter agent

--- ssm-parameter-store/main.tf
+++ ssm-parameter-store/main.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "testjson" {
-  name        = "test-json"
+  name        = "AmazonCloudWatch-Linux"
   description = "cloud watch json"
   type        = "String"
   value       = file("${path.module}/AmazonCloudWatch-Linux.json")
